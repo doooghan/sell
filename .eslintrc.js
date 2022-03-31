f47e8b3c086231e82546aa1a0d5af61daa0f2ae6
doooghan <doooghan@foxmail.com>
2022-03-31 20:33:49 +0800

    数据模拟和eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/multi-word-component-names": 0,
+    quotes: "off",
+    semi: "off",
+    "comma-dangle": "off",
     "space-before-function-paren": 0,
   },
   parserOptions: {
